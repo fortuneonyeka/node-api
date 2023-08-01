7d7456fd5c9af62c1263fd57d8d4c9c7d3787729
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-08-01 12:00:36 +0100

    fetch data from the database with the get request

--- server.js
+++ server.js
@@ -24,7 +24,20 @@ app.get("/", (req, res) => {
   res.send("hello API")
 })
 
-app.post("/product", async(req, res) => {
+app.get("/products", async(req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products)
+  } catch (error) {
+    console.log(error.message);
+  res.status(500).json({message: error.message})
+  }
+})
+
+
+
+
+app.post("/products", async(req, res) => {
  try {
   const product = await Product.create(req.body)
   res.status(200).json(product)
