ce2d329af97d08575766f65ce1d72574f3ed0cd5
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-08-02 17:06:02 +0100

    Revert "MVC"

--- server.js
+++ server.js
@@ -1,56 +1,91 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-
-const cors = require('cors')
-const productRoutes = require("./routes/productRoutes")
-const errorMiddleware = require("./middleware/errorMiddleware");
-require('dotenv').config()
-
-const MONGO_URL = process.env.MONGO_URL
-const PORT = process.env.PORT || 4996
-const FRONTEND = process.env.FRONTEND
-
-//enabling only specific domain or ip to access the backend
-// if you want multiple routes, put the routes in an array
-// example origin: ['http://example.com', 'http://localhost:4996']
-//else, set it as below for a single route
-
-// const corsOptions = {
-//   origin: FRONTEND,
-//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-// }
-
-
-
-//Simple Usage (Enable All CORS Requests)
-app.use(cors())
-
-//defined routes
-// app.use(cors(corsOptions))
-
+const Product = require("./models/productModel");
+const port = 4996;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(errorMiddleware)
-
-
 
 mongoose.set("strictQuery", false);
 mongoose
-  .connect(MONGO_URL)
+  .connect(
+    "mongodb+srv://admin:ogubuike4996@demoapi.sdxkjvv.mongodb.net/Node-API?retryWrites=true&w=majority"
+  )
   .then(() => {
     console.log("connected to mongoDB");
-    app.listen(PORT, () => {
-      console.log(`listening at port ${PORT}`);
+    app.listen(port, () => {
+      console.log(`listening at port ${port}`);
     });
   })
   .catch((error) => {
     console.log(`error message: ${error}`);
   });
 
- app.use("/api/products", productRoutes)
+app.post("/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
 
-  app.get("/", (req, res) => {
-    res.send("Welcome to my node API. This is a practice product API");
-  });
+app.get("/", (req, res) => {
+  res.send("hello API");
+});
 
+app.get("/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.put("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `The product with the ID ${id} does not exist` });
+    }
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.delete("/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if (!product) {
+      return res
+        .status(404)
+        .json({ message: `The product with the ID ${id} does not exist` });
+    }
+
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error.message);
+
+    res.status(500).json({ message: error.message });
+  }
+});
